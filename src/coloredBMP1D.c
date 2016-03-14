555eac886f1bd7989d278448f930cba04e308d19
ysiguman <victord765@gmail.com>
2016-03-14 08:00:18 +0100

    add border 1d Color for T,L,R

--- src/coloredBMP1D.c
+++ src/coloredBMP1D.c
@@ -2,12 +2,44 @@
 #include "../headers/header.h"
 
 
-void borderColor1D (color *tab, const int width, const int heigth)
+void borderColor1D (color *tab, const int width, const int height)
 {
 	int i = 0, j = 0;
-	printf("Hello World + %d + %d\n", width, heigth);
+	printf("Hello World + %d + %d\n", width, height);
 
-	
+	for (i = 0; i < 10; i = i + 1)
+	{
+		for (j = 0; j < width; ++j)
+		{
+			if (j >= 10 && i <= width - 10)
+			{
+				tab[i * height + j].b = 0;
+				tab[i * height + j].g = 255;
+				tab[i * height + j].r = 0;
+			}
+		}
+	}
+
+	for (i = 0; i < width * height; i = i + width)
+	{
+		for (j = 0; j < 10; ++j)
+		{
+			tab[i + j].b = 255;
+			tab[i + j].g = 255;
+			tab[i + j].r = 255;	
+			tab[i - j].b = 255;
+			tab[i - j].g = 255;
+			tab[i - j].r = 255;	
+			if (i >= 10 * width && i <= width * height - 10 * width)
+			{
+				tab[i + j].g = 0;
+				tab[i + j].r = 0;
+				tab[i - j].b = 0;
+				tab[i - j].g = 0;
+				tab[i - j].r = 0;	
+			}
+		}
+	}
 
 }
 
